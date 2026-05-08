3af8607fd8ac0bae618def255949e162af075391
dhunganasalina43 <dhunganasalina43@gmail.com>
2026-05-08 07:38:03 +0545

    fixing

--- SkillBridge.Api/Program.cs
+++ SkillBridge.Api/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,16 +8,17 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+// builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<SkillBridgeDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapScalarApiReference();
+   
 }
 
 app.UseHttpsRedirection();
@@ -25,3 +28,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
